51de809770f555c1cb9a46fa028e7e4eee8c7680
adutra <alexdut@gmail.com>
2012-05-16 17:17:49 +0200

    new message if stop fails.

--- src/main/java/de/flapdoodle/embedmongo/MongodProcess.java
+++ src/main/java/de/flapdoodle/embedmongo/MongodProcess.java
@@ -18,6 +18,8 @@
 package de.flapdoodle.embedmongo;
 
 import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -40,6 +42,13 @@ import de.flapdoodle.embedmongo.io.StreamConsumer;
 import de.flapdoodle.embedmongo.runtime.NUMA;
 import de.flapdoodle.embedmongo.runtime.ProcessShutdownWatcher;
 
+/**
+ * A wrapper around a Mongod {@link Process}.
+ * Provides a constructor that launches the process and
+ * a {@link #stop()} method to stop it.
+ * 
+ * @author Alexandre Dutra
+ */
 public class MongodProcess {
 
 	private static final Logger LOGGER = Logger.getLogger(MongodProcess.class.getName());
@@ -56,6 +65,12 @@ public class MongodProcess {
 
 	private StreamConsumer errStreamConsumer;
 	
+	/**
+	 * Create and launch a Mongod process according to the given {@link MongodConfig} and {@link MongodExecutable}.
+	 * @param mongodConfig
+	 * @param mongodExecutable
+	 * @throws IOException
+	 */
 	public MongodProcess(MongodConfig mongodConfig, MongodExecutable mongodExecutable) throws IOException {
 		
 		this.mongodExecutable = mongodExecutable;
@@ -79,7 +94,9 @@ public class MongodProcess {
 
 	}
 
-
+	/**
+	 * Stop the Mongod process.
+	 */
 	public synchronized void stop() {
 		if (!stopped) {
 			if (process != null) {
@@ -87,10 +104,15 @@ public class MongodProcess {
 					sendShutdownCommand();
 					waitForProcessStopped();
 					stopStreamConsumers();
-				} catch (IOException e) {
-					LOGGER.severe(e.getMessage());
 				} catch (InterruptedException e) {
 					LOGGER.severe(e.getMessage());
+				} catch (Exception e) {
+					String message = "\n\n" + "----------------------------------------------------\n"
+							+ "Something bad happend. We couldn't kill mongod process, and tried a lot.\n"
+							+ "If you want this problem solved you can help us if you open a new issue on github.\n" + "\n"
+							+ "Follow this link:\n" + "https://github.com/flapdoodle-oss/embedmongo.flapdoodle.de/issues\n" + "\n"
+							+ "Thank you:)\n" + "----------------------------------------------------\n\n";
+					LOGGER.log(Level.SEVERE, message, e);
 				} finally {
 					process.destroy();
 				}
@@ -105,14 +127,20 @@ public class MongodProcess {
 		}
 	}
 
+	/**
+	 * Build the process command line arguments.
+	 * @return
+	 * @throws IOException
+	 */
 	protected List<String> buildCommandLine() throws IOException {
 		List<String> ret = new ArrayList<String>();
+		File databaseDir = mongodConfig.getDatabaseDir();
 		ret.addAll(
 			Arrays.asList(
 				mongodExecutable.getFile().getAbsolutePath(), 
 				"-v", 
 				"--port", "" + mongodConfig.getPort(), 
-				"--dbpath", mongodConfig.getDatabaseDir().getAbsolutePath(), 
+				"--dbpath", databaseDir.getAbsolutePath(), 
 				"--noprealloc", 
 				"--nohttpinterface", 
 				"--smallfiles"));
@@ -130,7 +158,13 @@ public class MongodProcess {
 		return ret;
 	}
 
+	/**
+	 * Start the registered {@link StreamConsumer}s.
+	 * @return a {@link ProcessStartListener} to watch the process output and detect a successful startup.
+	 * @throws UnsupportedEncodingException if the encoding specified by {@link MongodConfig} is unsupported.
+	 */
 	protected ProcessStartListener startStreamConsumers() throws UnsupportedEncodingException {
+		//special listener to detect process startup
 		ProcessStartListener startListener = new ProcessStartListener();
 		//standard output
 		BufferedReader stdReader = new BufferedReader(new InputStreamReader(process.getInputStream(), mongodConfig.getEncoding()), mongodConfig.getBufferLength());
@@ -150,22 +184,42 @@ public class MongodProcess {
 		return startListener;
 	}
 
-	protected void sendShutdownCommand() throws UnknownHostException {
+
+	/**
+	 * Shutdown the mongod process.
+	 * The current implementation uses the java client to send a "shutdown" command to the server.
+	 * @return
+	 * @throws UnknownHostException in the (very unlikely) situation where "127.0.0.1" cannot be resolved. 
+	 * @throws MongoException if the java driver throws an unexpected exception while connecting to the Mongod process.
+	 */
+	protected void sendShutdownCommand() throws MongoException, UnknownHostException {
 		//do NOT use "localhost", we need the raw IP to get through without authentication
 		Mongo mongo = new Mongo("127.0.0.1", mongodConfig.getPort());
-		//annoying: the driver prints the whole stack trace
+		//annoying: the driver gets an EOFException because the connection 
+		//is closed by the server and prints the whole stack trace to the console
 		Logger logger = Logger.getLogger("com.mongodb");
 		Level level = logger.getLevel();
 		try {
 			logger.setLevel(Level.OFF);
 			mongo.getDB("admin").command(new BasicDBObject("shutdown",1));
-		} catch (MongoException e) {
+		} catch (MongoException.Network e) {
+			if(e.getCause() != null && e.getCause() instanceof EOFException) {
+				//OK: the driver is complaining about the server closing the connection abruptly
+			} else {
+				throw e;
+			}
 		} finally {
 			logger.setLevel(level);
+			//FIXME Win XP: cleaner threads are not always properly stopped, seems to be a bug in the driver
+			//see https://jira.mongodb.org/browse/JAVA-306
 			mongo.close();
 		}
 	}
 
+	/**
+	 * Stop the registered {@link StreamConsumer}. Await for the threads to terminate unless a timeout happens.
+	 * @throws InterruptedException
+	 */
 	protected void stopStreamConsumers() throws InterruptedException {
 		if(stdStreamConsumer != null) {
 			stdStreamConsumer.join(mongodConfig.getShutdownTimeout());
@@ -175,6 +229,10 @@ public class MongodProcess {
 		}
 	}
 
+	/**
+	 * Await for the process to terminate normally unless a timeout happens..
+	 * @throws InterruptedException
+	 */
 	protected void waitForProcessStopped() {
 		ProcessShutdownWatcher t = new ProcessShutdownWatcher(process);
 		t.start();
@@ -188,6 +246,11 @@ public class MongodProcess {
 	}
 
 
+	/**
+	 * Special {@link IStreamListener} that inspects the process output
+	 * looking for the message stating that the process is successfully started.
+	 * 
+	 */
 	private static class ProcessStartListener implements IStreamListener {
 
 		private static final String SUCCESS_MSG = "waiting for connections on port";
@@ -206,6 +269,12 @@ public class MongodProcess {
 			}
 		}
 
+		/**
+		 * Block until the success message is detected in the Mongod process output,
+		 * or if a timeout occurs, whichever happens first.
+		 * @param timeout the timeout in milliseconds
+		 * @return true if the success message was detected, false otherwise.
+		 */
 		public boolean waitForProcessStarted(long timeout) {
 			try {
 				latch.await(timeout, TimeUnit.MILLISECONDS);
@@ -217,6 +286,10 @@ public class MongodProcess {
 
 	}
 	
+	/**
+	 * Shtudown hook to stop the Mongod process
+	 * by calling its {@link MongodProcess#stop()} method.
+	 */
 	private class MongodProcessStopper extends Thread {
 		@Override
 		public void run() {
