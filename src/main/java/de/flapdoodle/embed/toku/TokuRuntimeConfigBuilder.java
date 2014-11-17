6493cbfc7212d634122f8693395d40d3f2075577
Ben McCann <ben@connectifier.com>
2014-11-17 13:29:21 -0800

    Allow for also creating an embedded TokuMX (popular fork of MongoDB)

---
+++ src/main/java/de/flapdoodle/embed/toku/TokuRuntimeConfigBuilder.java
@@ -0,0 +1,13 @@
+package de.flapdoodle.embed.toku;
+
+import de.flapdoodle.embed.mongo.Command;
+import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder;
+
+public class TokuRuntimeConfigBuilder extends RuntimeConfigBuilder {
+
+    public TokuRuntimeConfigBuilder defaults(Command command) {
+        super.defaults(command);
+        artifactStore().overwriteDefault(new TokuArtifactStoreBuilder().defaults(command).build());
+        return this;
+    }
+}
